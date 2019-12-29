3690ea69b2e7bfa70bb7d11f1d019220813b87c4
ankuradlakha <ankur.adlakha@hedgehoglab.com>
2019-12-29 18:00:40 +0530

    +Added backgrounds +Added ad view

--- app/src/main/java/com/bluexpresso/materialplayground/MainActivity.kt
+++ app/src/main/java/com/bluexpresso/materialplayground/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.bluexpresso.materialplayground.databinding.ActivityMainBinding
 import com.bluexpresso.materialplayground.materialcomponents.MaterialComponentsActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
                 as ActivityMainBinding
         initMaterialComponents(binding)
+        initAds(binding.adView)
     }
 
     private fun initMaterialComponents(binding: ActivityMainBinding) {
@@ -21,5 +25,14 @@ class MainActivity : AppCompatActivity() {
         adapter.setOnItemClickListener {
             MaterialComponentsActivity.startActivity(this@MainActivity, it)
         }
+    }
+
+    private fun initAds(adView: AdView) {
+        MobileAds.initialize(this) {
+        }
+        adView.loadAd(
+            AdRequest.Builder()
+                .build()
+        )
     }
 }
--- app/src/main/java/com/bluexpresso/materialplayground/materialcomponents/MaterialComponentsActivity.kt
+++ app/src/main/java/com/bluexpresso/materialplayground/materialcomponents/MaterialComponentsActivity.kt
@@ -8,6 +8,9 @@ import androidx.databinding.DataBindingUtil
 import com.bluexpresso.materialplayground.MaterialComponents
 import com.bluexpresso.materialplayground.R
 import com.bluexpresso.materialplayground.databinding.ActivityMaterialComponentsBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MaterialComponentsActivity : AppCompatActivity() {
     companion object {
@@ -21,14 +24,29 @@ class MaterialComponentsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView(this, R.layout.activity_material_components)
+        val binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_material_components)
                 as ActivityMaterialComponentsBinding
         if (savedInstanceState == null && intent.extras != null) {
             val ft = supportFragmentManager.beginTransaction()
-            ft.replace(R.id.fragment_container,MaterialComponents.getNavigationForItem(
-                intent.extras!!.getString(ARG_COMPONENT_TITLE,
-                    MaterialComponents.APP_BARS_BOTTOM.title)
-            )).commit()
+            ft.replace(
+                R.id.fragment_container, MaterialComponents.getNavigationForItem(
+                    intent.extras!!.getString(
+                        ARG_COMPONENT_TITLE,
+                        MaterialComponents.APP_BARS_BOTTOM.title
+                    )
+                )
+            ).commit()
         }
+        initAds(binding.adView)
+    }
+
+    private fun initAds(adView: AdView) {
+        MobileAds.initialize(this) {
+        }
+        adView.loadAd(
+            AdRequest.Builder()
+                .build()
+        )
     }
 }
